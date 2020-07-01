95063e057409d116d136d6fe4d1c78517ac5f145
voyageTheBest <44881592+voyageTheBest@users.noreply.github.com>
2020-07-01 04:41:54 -0700

    import initial version

---
+++ app/src/main/java/com/tufts/navigationv2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.tufts.navigationv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.dashboard, R.id.profile, R.id.training, R.id.tobyStart, R.id.home2)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    public void GoSetting(View view) {
+        Intent intent = new Intent(this, setting.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/tufts/navigationv2/setting.java
@@ -0,0 +1,14 @@
+package com.tufts.navigationv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
